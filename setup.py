46b3f5538cf0101a35d5855f70f3fbbed58ea0ec
Haoling (Horus) Zhang <hlzchn@gmail.com>
2022-12-27 15:11:08 +0800

    add cases

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name="storeall",
+    name="PyMOL-advance",
     version="0.0",
-    description="PyMOL-advance: high-level interfaces from structure data to manuscript-level figures",
+    description="PyMOL-advance: high-level interfaces from structure data to manuscript-level expected_figures",
     author="Haoling ZHANG",
     author_email="zhanghaoling@genomics.cn",
     url="https://github.com/BGI-SynBio/PyMOL-advance",
-    packages=["PyMOL-advance.mola", "PyMOL-advance.tests"],
+    packages=["PyMOL-advance.mola", "PyMOL-advance.tests", "PyMOL-advance.cases"],
     install_requires=["numpy"],
     classifiers=["Programming Language :: Python :: 3",
                  "Operating System :: OS Independent"],
