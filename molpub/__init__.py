163ae523938ad9b8a8a669f50a88eb3db62e9081
ytchen-1 <101387893+ytchen-1@users.noreply.github.com>
2023-10-24 16:57:59 +0800

    fix bug

--- molpub/__init__.py
+++ molpub/__init__.py
@@ -7,5 +7,5 @@ from os import path
 
 # load required font formats.
 font_files = font_manager.findSystemFonts(path.abspath(__file__).replace("\\", "/")[:-11] + "fonts/")
-font_list = font_manager.createFontList(font_files)
-font_manager.fontManager.ttflist.extend(font_list)
+for font_file in font_files:
+    font_manager.fontManager.addfont(font_file)
